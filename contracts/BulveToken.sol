717d54dee98f91f0edb88817865c3a1b4861b794
renderTom <Tomas@rendertom.com>
2021-11-29 11:59:05 +0200

    chore(contracts): build main token contracts

---
+++ contracts/BulveToken.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.5.0;
+
+contract BulveToken {
+  string public name = "Bulve Token";
+  string public symbol = "BULVE";
+  uint256 public totalSupply = 1000000;
+
+  event Transfer(
+    address _from,
+    address _to,
+    uint256 _value
+  );
+
+  mapping(address => uint256) public balanceOf;
+
+  constructor() public {
+    balanceOf[msg.sender] = totalSupply;
+  }
+
+  function transfer(address _to, uint256 _value) public returns (bool success) {
+    require(balanceOf[msg.sender] >= _value, "Value exceeds the owner balance");
+    balanceOf[msg.sender] -= _value;
+    balanceOf[_to] += _value;
+    emit Transfer(msg.sender, _to, _value);
+    return true;
+  }
+}
