e60c88a306bfe40367e6cde43e872cd2392000e3
Tomas Sinkunas <Tomas@rendertom.com>
2021-12-04 21:16:14 +0200

    app(): start adding elements

--- client/src/App.js
+++ client/src/App.js
@@ -1,73 +1,139 @@
-import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import React, { useEffect, useState } from "react";
 import getWeb3 from "./getWeb3";
+
+import BulveToken from "./contracts/BulveToken.json";
+import EurToken from "./contracts/EurToken.json";
+import Farm from "./contracts/Farm.json";
+import MorkaToken from "./contracts/MorkaToken.json";
+import RidikasToken from "./contracts/RidikasToken.json";
 
 import "./App.css";
 
-class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+const App = () => {
+  const [account, setAccount] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
-  componentDidMount = async () => {
-    try {
-      // Get network provider and web3 instance.
+  const [bulve, setBulve] = useState({ contract: null, balance: null });
+  const [eur, setEur] = useState({ contract: null, balance: null });
+  const [morka, setMorka] = useState({ contract: null, balance: null });
+  const [ridikas, setRidikas] = useState({ contract: null, balance: null });
+  const [farm, setFarm] = useState({ contract: null, balance: null });
+
+  useEffect(() => {
+    init().catch(console.error);
+    async function init() {
       const web3 = await getWeb3();
 
-      // Use web3 to get the user's accounts.
       const accounts = await web3.eth.getAccounts();
+      const account = accounts[0];
+      setAccount(account);
 
-      // Get the contract instance.
       const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
-      );
 
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
-    } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
-      );
-      console.error(error);
+      const balanceOf = (contract, _account = account) => contract.methods.balanceOf(_account).call();
+      const getStakingBalance = (contract, currency) => contract.methods.getUserBalance(currency).call();
+      const initContract = (_token) => {
+        const address = _token.networks[networkId];
+        return new web3.eth.Contract(_token.abi, address.address, { from: account });
+      }
+
+      const [bulve, eur, farm, morka, ridikas] = await Promise.all([
+        initContract(BulveToken),
+        initContract(EurToken),
+        initContract(Farm),
+        initContract(MorkaToken),
+        initContract(RidikasToken),
+      ]);
+
+      const getContractData = async (contract) => {
+        return {
+          contract: contract,
+          balance: await balanceOf(contract),
+          left: await balanceOf(contract, farm._address),
+          totalSupply: await contract.methods.totalSupply().call(),
+          name: await contract.methods.name().call(),
+          symbol: await contract.methods.symbol().call(),
+        }
+      }
+
+      setBulve(await getContractData(bulve));
+      setEur(await getContractData(eur));
+      setMorka(await getContractData(morka));
+      setRidikas(await getContractData(ridikas));
+      setFarm({
+        contract: farm,
+        balance: {
+          BULVE: await getStakingBalance(farm, 'BULVE'),
+          MORKA: await getStakingBalance(farm, 'MORKA'),
+          RIDIKAS: await getStakingBalance(farm, 'RIDIKAS')
+        }
+      });
+
+      setIsLoading(false);
     }
-  };
+  }, []);
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
-
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
-  };
-
-  render() {
-    if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
-    }
-    return (
-      <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 42</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
-      </div>
-    );
+  const stakeTokens = (amount, currency) => {
+    setIsLoading(true);
+    farm.contract.methods.stakeTokens(amount, currency).send({ from: account }).on('transactionHash', (hash) => {
+      setIsLoading(false);
+    })
   }
+
+  const unstakeTokens = (currency) => {
+    setIsLoading(true);
+    farm.contract.methods.unstakeTokens(currency).send({ from: account }).on('transactionHash', (hash) => {
+      setIsLoading(false);
+    })
+  }
+
+  const issueTokens = (currency) => {
+    setIsLoading(true);
+    farm.contract.methods.issueTokens(currency).send({ from: account }).on('transactionHash', (hash) => {
+      setIsLoading(false);
+    })
+  }
+
+  if (isLoading) {
+    return <h1>loading...</h1>
+  }
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => stakeTokens(10, 'BULVE')}>stake bulve tokens</button>
+        <button onClick={() => stakeTokens(10, 'MORKA')}>stake morka tokens</button>
+        <button onClick={() => stakeTokens(1, 'RIDIKAS')}>stake ridikas tokens</button>
+      </div>
+      <div>
+        <button onClick={() => unstakeTokens('BULVE')}>unstake bulve tokens</button>
+        <button onClick={() => unstakeTokens('MORKA')}>unstake morka tokens</button>
+        <button onClick={() => unstakeTokens('RIDIKAS')}>unstake ridikas tokens</button>
+      </div>
+      <div>
+        <button onClick={() => issueTokens('BULVE')}>issue bulve tokens</button>
+        <button onClick={() => issueTokens('MORKA')}>issue morka tokens</button>
+        <button onClick={() => issueTokens('RIDIKAS')}>issue ridikas tokens</button>
+      </div>
+      <p>User {account}</p>
+      <img alt="" id='avatar' src={'https://avatars.dicebear.com/api/avataaars/' + account + '.svg'} />
+      <p>Eurai: {eur.balance}</p>
+      <div>
+        {
+          [bulve, morka, ridikas].map((item, index) => {
+            return (
+              <p key={index}>
+                {item.name}: staking balance: {farm.balance[item.symbol]} (EUR)
+                rewards: {item.balance}({item.symbol})
+                left: {item.left}
+                totalSupply: {item.totalSupply}
+              </p>
+            )
+          })
+        }
+      </div>
+    </div>
+  )
 }
 
 export default App;
