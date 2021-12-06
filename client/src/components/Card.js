37a23437291fb8fd44f097b98cf8b4ec6ef386f1
Tomas Sinkunas <Tomas@rendertom.com>
2021-12-06 09:27:10 +0200

    styles(css): separate CSS from Component

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,32 +1,17 @@
 import React from 'react';
 import Button from './Button.js';
+
+import styles from './Card.module.css';
 const assets = require('../utils/assets.js')
 
 const Card = ({symbol, name, totalSupply, issued, onIssueTokensClick}) => {
-  const styles = {
-    card: {
-      backgroundColor: "white",
-      borderRadius: "20px",
-      margin: "0 50px",
-      padding: "20px",
-      position: "relative",
-    },
-    icon: {
-      width: "50px",
-      height: "50px",
-      position: "absolute",
-      top: "-25px",
-      right: "-25px",
-    }
-  }
-
   return (
-    <div style={styles.card}>
+    <div className={styles.card}>
       <h3>{symbol.toUpperCase()}</h3>
       <p>{name}</p>
       <h5>total supply: {totalSupply}</h5>
       <h5>available: {issued}</h5>
-      <img style={styles.icon} src={assets[symbol.toLowerCase()]} alt={symbol.toLowerCase()} />
+      <img className={styles.icon} src={assets[symbol.toLowerCase()]} alt={symbol.toLowerCase()} />
       <Button title="issue" onClick={onIssueTokensClick}/>
     </div>
   )
