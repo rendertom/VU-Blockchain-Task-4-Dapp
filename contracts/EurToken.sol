34e6930d740ddca5d7ba561c90533d106e2f9248
Tomas Sinkunas <Tomas@rendertom.com>
2021-12-07 19:11:02 +0200

    contract(eur): expose owner

--- contracts/EurToken.sol
+++ contracts/EurToken.sol
@@ -4,6 +4,7 @@ contract EurToken {
   string public name = "Stable Euro Token";
   string public symbol = "EUR";
   uint256 public totalSupply = 1000000;
+  address public owner;
 
   event Transfer(
     address _from,
@@ -14,11 +15,12 @@ contract EurToken {
   mapping(address => uint256) public balanceOf;
 
   constructor() public {
-    balanceOf[msg.sender] = totalSupply;
+    owner = msg.sender;
+    balanceOf[owner] = totalSupply;
   }
 
   function transfer(address _to, uint256 _value) public returns (bool success) {
-    require(balanceOf[msg.sender] >= _value, "transferFrom (EUR): Value exceeds the owner balance");
+    require(balanceOf[msg.sender] >= _value, "transfer (EUR): Value exceeds the owner balance");
     balanceOf[msg.sender] -= _value;
     balanceOf[_to] += _value;
     emit Transfer(msg.sender, _to, _value);
